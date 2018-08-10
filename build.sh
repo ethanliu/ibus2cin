1d6971ab6f7fa22f1c8dc0a7d674d45d2053d665
Ethan Liu <ethan@creativecrap.com>
2018-08-11 04:51:47 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -8,17 +8,23 @@ for GOOS in darwin linux windows; do
 	for GOARCH in 386 amd64; do
 		echo "Building $GOOS-$GOARCH"
 
-		DISTPATH="dist/${GOOS}/${GOARCH}"
+		DISTPATH=
 
 		if [ "${GOOS}" == "windows" ]; then
 			if [ "${GOARCH}" == "386" ]; then
-				env GOOS="${GOOS}" GOARCH="${GOARCH}" CGO_ENABLED=1 CC="/usr/local/opt/mingw-w64/bin/i686-w64-mingw32-gcc" go build -o "${DISTPATH}/ibus2cin.exe"
+				env GOOS="${GOOS}" GOARCH="${GOARCH}" CGO_ENABLED=1 CC="/usr/local/opt/mingw-w64/bin/i686-w64-mingw32-gcc" go build -o "dist/ibus2cin.exe"
 			else
-				env GOOS="${GOOS}" GOARCH="${GOARCH}" CGO_ENABLED=1 CC="/usr/local/opt/mingw-w64/bin/x86_64-w64-mingw32-gcc" go build -o "${DISTPATH}/ibus2cin.exe"
+				env GOOS="${GOOS}" GOARCH="${GOARCH}" CGO_ENABLED=1 CC="/usr/local/opt/mingw-w64/bin/x86_64-w64-mingw32-gcc" go build -o "dist/ibus2cin.exe"
 			fi
+			tar zcf "dist/ibus2cin-${GOOS}-${GOARCH}.tar.gz" "dist/ibus2cin.exe"
 		else
-			env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o "${DISTPATH}/ibus2cin"
+			env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o "dist/ibus2cin"
+			tar zcf "dist/ibus2cin-${GOOS}-${GOARCH}.tar.gz" "dist/ibus2cin"
 		fi
+
 
 	done
 done
+
+rm dist/ibus2cin.exe
+rm dist/ibus2cin
